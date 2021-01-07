80d9191f1930faf4e8e8b24cc610c3b81f7ead76
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2021-01-07 13:24:41 +0100

    Fix copyright
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/test/java/org/gridsuite/modification/server/GroovyScriptResultTest.java
+++ src/test/java/org/gridsuite/modification/server/GroovyScriptResultTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.modification.server;
 
 import com.google.common.collect.ImmutableSet;
@@ -9,6 +15,9 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
+ */
 public class GroovyScriptResultTest {
 
     @Test
