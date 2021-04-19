49a9478adfee5d617f511f8c5e87fba9d6638890
Slimane AMAR <slimane.amar@rte-france.com>
2021-04-19 14:08:03 +0200

    Add a listener SLF4J to log queries (#17)
    Signed-off-by: Slimane AMAR <slimane.amar@rte-france.com>

--- src/test/java/org/gridsuite/modification/server/utils/DatasourceProxyBeanPostProcessor.java
+++ src/test/java/org/gridsuite/modification/server/utils/DatasourceProxyBeanPostProcessor.java
@@ -1,22 +1,27 @@
-package org.gridsuite.modification.server;
+package org.gridsuite.modification.server.utils;
 
 import javax.sql.DataSource;
 
 import net.ttddyy.dsproxy.listener.ChainListener;
 import net.ttddyy.dsproxy.listener.DataSourceQueryCountListener;
+import net.ttddyy.dsproxy.listener.logging.SLF4JQueryLoggingListener;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 @Component
-// Decorator datasource bug
+// Decorator datasource init bug with original method (Vlad Mihalcea) : circular dependency
 // See https://vladmihalcea.com/how-to-detect-the-n-plus-one-query-problem-during-testing
+// Use this instead
 public class DatasourceProxyBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessAfterInitialization(final Object bean, final String beanName) throws BeansException {
         if (bean instanceof DataSource) {
             ChainListener listener = new ChainListener();
+            SLF4JQueryLoggingListener loggingListener = new SLF4JQueryLoggingListener();
+            loggingListener.setQueryLogEntryCreator(new InlineQueryLogEntryCreator());
+            listener.addListener(loggingListener);
             listener.addListener(new DataSourceQueryCountListener());
             return ProxyDataSourceBuilder
                     .create((DataSource) bean)
