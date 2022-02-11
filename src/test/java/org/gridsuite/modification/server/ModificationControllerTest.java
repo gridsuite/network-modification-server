285b4b88904414603552dc753cdc034218427e99
Franck LECUYER <franck.lecuyer@rte-france.com>
2022-02-11 16:55:32 +0100

    Remove the dangling switches when deleting an equipment. Fix some topological errors in the network used for unit tests, so the delete equipments test succeeds. Add specific unit test with error in removing dangling switches.
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/test/java/org/gridsuite/modification/server/ModificationControllerTest.java
+++ src/test/java/org/gridsuite/modification/server/ModificationControllerTest.java
@@ -1224,6 +1224,26 @@ public class ModificationControllerTest {
     }
 
     @Test
+    public void testErrorRemovingDanglingSwitches() {
+        String uriString = "/v1/networks/{networkUuid}/equipments/type/{equipmentType}/id/{equipmentId}?group=" + TEST_GROUP_ID;
+
+        // delete load with error removing dangling switches, because the load connection node is not linked to any other node
+        webTestClient.delete().uri(uriString, TEST_NETWORK_ID, "LOAD", "v5load")
+            .exchange()
+            .expectStatus().is5xxServerError()
+            .expectBody(String.class)
+            .value(exception -> exception, containsString("DELETE_EQUIPMENT_ERROR : no such vertex in graph: 2"));
+
+        // no modifications added
+        webTestClient.get().uri("/v1/groups")
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .expectBodyList(UUID.class)
+            .isEqualTo(List.of());
+    }
+
+    @Test
     public void testCreateLineInNodeBreaker() {
         String uriString = "/v1/networks/{networkUuid}/lines?group=" + TEST_GROUP_ID;
 
