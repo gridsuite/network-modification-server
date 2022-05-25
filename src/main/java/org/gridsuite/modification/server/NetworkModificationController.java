bdc04e43128360a7894f7247784c07c2027fc67c
Hugo Marcellin <hugo.marcelin@rte-france.com>
2022-05-25 14:17:23 +0200

    Implement PR remarks

--- src/main/java/org/gridsuite/modification/server/NetworkModificationController.java
+++ src/main/java/org/gridsuite/modification/server/NetworkModificationController.java
@@ -71,7 +71,8 @@ public class NetworkModificationController {
 
     @PostMapping(value = "/groups")
     @Operation(summary = "Create a group based on another group and its modifications")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "The group and its modifications have been duplicated")})
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "The group and its modifications have been duplicated"),
+                           @ApiResponse(responseCode = "404", description = "Source group not found")})
     public ResponseEntity<Mono<Void>> createGroup(@RequestParam("groupUuid") UUID groupUuid,
                                                   @RequestParam("duplicateFrom") UUID sourceGroupUuid,
                                                   @RequestParam("reportUuid") UUID reportUuid) {
