0ac1702630d0838aeb7237e90341f2591553dfa9
Hugo Marcellin <marcellinhugo@orange.fr>
2022-06-30 10:41:11 +0200

    Fix modification group duplication (#101)
    * Fixing modification group duplication issue
    Signed-off-by: Hugo Marcellin <hugo.marcelin@rte-france.com>

--- src/main/java/org/gridsuite/modification/server/service/NetworkModificationService.java
+++ src/main/java/org/gridsuite/modification/server/service/NetworkModificationService.java
@@ -215,12 +215,10 @@ public class NetworkModificationService {
         return Flux.fromStream(() -> networkModificationRepository.getModifications(List.of(modificationUuid)).stream());
     }
 
-    public Mono<Void> createGroup(UUID sourceGroupUuid, UUID groupUuid, UUID reportUuid) {
-        return getModifications(sourceGroupUuid, false, true).doOnNext(m -> {
+    public Mono<Void> createModificationGroup(UUID sourceGroupUuid, UUID groupUuid, UUID reportUuid) {
+        //TODO To be optimized by retrieving ModificationEntity objects directly instead of ModificationInfos objects
+        return getModifications(sourceGroupUuid, false, false).doOnNext(m -> {
             Optional<ModificationEntity> modification = this.modificationRepository.findById(m.getUuid());
-            if (modification.isEmpty()) {
-                throw new NetworkModificationException(MODIFICATION_NOT_FOUND, m.getUuid().toString());
-            }
             modification.get().setId(null);
             networkModificationRepository.saveModifications(groupUuid, List.of(modification.get()));
         }).doOnNext(unused -> {
