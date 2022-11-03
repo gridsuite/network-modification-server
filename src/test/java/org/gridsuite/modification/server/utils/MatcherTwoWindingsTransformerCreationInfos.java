3edc208f548dd9f56163426a8c1e9ca2db0dcfea
Hugo Marcellin <hugo.marcelin@rte-france.com>
2022-11-03 10:51:57 +0100

    PR suggestions

--- src/test/java/org/gridsuite/modification/server/utils/MatcherTwoWindingsTransformerCreationInfos.java
+++ src/test/java/org/gridsuite/modification/server/utils/MatcherTwoWindingsTransformerCreationInfos.java
@@ -12,6 +12,8 @@ import org.gridsuite.modification.server.dto.RatioTapChangerCreationInfos;
 import org.gridsuite.modification.server.dto.TwoWindingsTransformerCreationInfos;
 import org.hamcrest.Description;
 
+import java.util.Objects;
+
 /**
  * @author Abdelsalem Hedhili <abdelsalem.hedhili at rte-france.com>
  */
@@ -25,20 +27,20 @@ public class MatcherTwoWindingsTransformerCreationInfos extends MatcherModificat
         super(ref);
     }
 
-    public boolean matchesCurrentLimitsInfos(CurrentLimitsInfos cl1, CurrentLimitsInfos cl2) {
+    private boolean matchesCurrentLimitsInfos(CurrentLimitsInfos cl1, CurrentLimitsInfos cl2) {
         return (cl1 == null && cl2 == null)
                 || (cl1 != null && cl2 != null && cl1.getPermanentLimit() != null && cl2.getPermanentLimit() != null && cl1.getPermanentLimit().equals(cl2.getPermanentLimit()))
                 || (cl1 != null && cl2 != null && cl1.getPermanentLimit() == null && cl2.getPermanentLimit() == null);
     }
 
-    public boolean matchesRatioTapChangerInfos(RatioTapChangerCreationInfos rt1, RatioTapChangerCreationInfos rt2) {
+    private boolean matchesRatioTapChangerInfos(RatioTapChangerCreationInfos rt1, RatioTapChangerCreationInfos rt2) {
         return (rt1 == null && rt2 == null)
-                || (rt1 != null && rt2 != null && rt1.isLoadTapChangingCapabilities() == rt2.isLoadTapChangingCapabilities() && rt1.isRegulating() == rt2.isRegulating() && areDoublesEqual(rt1.getTargetV(), rt2.getTargetV()) && areDoublesEqual(rt1.getTargetDeadband(), rt2.getTargetDeadband()) && areStringsEqual(rt1.getRegulatingTerminalVlId(), rt2.getRegulatingTerminalVlId()) && areStringsEqual(rt1.getRegulatingTerminalType(), rt2.getRegulatingTerminalType()) && areStringsEqual(rt1.getRegulatingTerminalId(), rt2.getRegulatingTerminalId()) && rt1.getLowTapPosition() == rt2.getLowTapPosition() && rt1.getTapPosition() == rt2.getTapPosition());
+                || (rt1 != null && rt2 != null && rt1.isLoadTapChangingCapabilities() == rt2.isLoadTapChangingCapabilities() && rt1.isRegulating() == rt2.isRegulating() && areDoublesEqual(rt1.getTargetV(), rt2.getTargetV()) && areDoublesEqual(rt1.getTargetDeadband(), rt2.getTargetDeadband()) && Objects.equals(rt1.getRegulatingTerminalVlId(), rt2.getRegulatingTerminalVlId()) && Objects.equals(rt1.getRegulatingTerminalType(), rt2.getRegulatingTerminalType()) && Objects.equals(rt1.getRegulatingTerminalId(), rt2.getRegulatingTerminalId()) && rt1.getLowTapPosition() == rt2.getLowTapPosition() && rt1.getTapPosition() == rt2.getTapPosition());
     }
 
-    public boolean matchesPhaseTapChangerInfos(PhaseTapChangerCreationInfos pt1, PhaseTapChangerCreationInfos pt2) {
+    private boolean matchesPhaseTapChangerInfos(PhaseTapChangerCreationInfos pt1, PhaseTapChangerCreationInfos pt2) {
         return (pt1 == null && pt2 == null)
-                || (pt1 != null && pt2 != null && pt1.getRegulationMode().equals(pt2.getRegulationMode()) && pt1.isRegulating() == pt2.isRegulating() && pt1.getRegulationMode() == pt2.getRegulationMode() && areDoublesEqual(pt1.getRegulationValue(), pt2.getRegulationValue()) && areDoublesEqual(pt1.getTargetDeadband(), pt2.getTargetDeadband()) && areStringsEqual(pt1.getRegulatingTerminalVlId(), pt2.getRegulatingTerminalVlId()) && areStringsEqual(pt1.getRegulatingTerminalType(), pt2.getRegulatingTerminalType()) && areStringsEqual(pt1.getRegulatingTerminalId(), pt2.getRegulatingTerminalId()) && pt1.getLowTapPosition() == pt2.getLowTapPosition() && pt1.getTapPosition() == pt2.getTapPosition());
+                || (pt1 != null && pt2 != null && pt1.getRegulationMode().equals(pt2.getRegulationMode()) && pt1.isRegulating() == pt2.isRegulating() && pt1.getRegulationMode() == pt2.getRegulationMode() && areDoublesEqual(pt1.getRegulationValue(), pt2.getRegulationValue()) && areDoublesEqual(pt1.getTargetDeadband(), pt2.getTargetDeadband()) && Objects.equals(pt1.getRegulatingTerminalVlId(), pt2.getRegulatingTerminalVlId()) && Objects.equals(pt1.getRegulatingTerminalType(), pt2.getRegulatingTerminalType()) && Objects.equals(pt1.getRegulatingTerminalId(), pt2.getRegulatingTerminalId()) && pt1.getLowTapPosition() == pt2.getLowTapPosition() && pt1.getTapPosition() == pt2.getTapPosition());
     }
 
     @Override
@@ -64,12 +66,8 @@ public class MatcherTwoWindingsTransformerCreationInfos extends MatcherModificat
                 && matchesPhaseTapChangerInfos(m.getPhaseTapChanger(), reference.getPhaseTapChanger());
     }
 
-    public boolean areDoublesEqual(Double a, Double b) {
-        return (a == null && b == null) || (a != null && b != null && a.equals(b));
-    }
-
-    public boolean areStringsEqual(String a, String b) {
-        return (a == null && b == null) || (a != null && a.equals(b));
+    private boolean areDoublesEqual(Double a, Double b) {
+        return (a == null && b == null) || (a != null && b != null && (Math.abs(a - b) < EPSILON));
     }
 
     @Override
