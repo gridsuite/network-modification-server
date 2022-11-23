421c1c6c0e1fabc8729cd381a64f7e2175187558
David BRAQUART <david.braquart@rte-france.com>
2022-11-23 18:26:16 +0100

    hack to be able to create a 2WT having deadband with null optional value

--- src/main/java/org/gridsuite/modification/server/service/NetworkModificationService.java
+++ src/main/java/org/gridsuite/modification/server/service/NetworkModificationService.java
@@ -1166,6 +1166,32 @@ public class NetworkModificationService {
         }
     }
 
+    class NullDeadBandChecker {
+        private boolean nullForRatioTapChanger = false;
+        private boolean nullForPhaseTapChanger = false;
+        private static final double DEADBAND_NULL_VALUE = 0.;
+
+        public NullDeadBandChecker(TwoWindingsTransformerCreationInfos twoWindingsTransformerCreationInfos) {
+            if (twoWindingsTransformerCreationInfos.getRatioTapChanger() != null && twoWindingsTransformerCreationInfos.getRatioTapChanger().getTargetDeadband() == null) {
+                nullForRatioTapChanger = true;
+                twoWindingsTransformerCreationInfos.getRatioTapChanger().setTargetDeadband(DEADBAND_NULL_VALUE);
+            }
+            if (twoWindingsTransformerCreationInfos.getPhaseTapChanger() != null && twoWindingsTransformerCreationInfos.getPhaseTapChanger().getTargetDeadband() == null) {
+                nullForPhaseTapChanger = true;
+                twoWindingsTransformerCreationInfos.getPhaseTapChanger().setTargetDeadband(DEADBAND_NULL_VALUE);
+            }
+        }
+
+        public void restore(TwoWindingsTransformerCreationInfos twoWindingsTransformerCreationInfos) {
+            if (nullForRatioTapChanger) {
+                twoWindingsTransformerCreationInfos.getRatioTapChanger().setTargetDeadband(null);
+            }
+            if (nullForPhaseTapChanger) {
+                twoWindingsTransformerCreationInfos.getPhaseTapChanger().setTargetDeadband(null);
+            }
+        }
+    }
+
     private List<ModificationInfos> execCreateTwoWindingsTransformerCreation(NetworkStoreListener listener, TwoWindingsTransformerCreationInfos twoWindingsTransformerCreationInfos,
                                                                      UUID reportUuid, String reporterId) {
         Network network = listener.getNetwork();
@@ -1176,6 +1202,13 @@ public class NetworkModificationService {
 
         return doAction(listener, () -> {
             if (listener.isApplyModifications()) {
+                // Next is a hack to allow the creation of a 2WT without a specific deadband (null). As the network-store
+                // currently does not accept null value, we just pass 0 here before the equipment creation in the network,
+                // cf addXXXTransformer() below.
+                // Later we switch back to null, so the network modification still has null value in its entity.
+                // TODO This hack should be removed when Powsybl will support optional value for the deadband.
+                NullDeadBandChecker nullDeadBandChecker = new NullDeadBandChecker(twoWindingsTransformerCreationInfos);
+
                 // create the 2wt in the network
                 VoltageLevel voltageLevel1 = getVoltageLevel(network, twoWindingsTransformerCreationInfos.getVoltageLevelId1());
                 VoltageLevel voltageLevel2 = getVoltageLevel(network, twoWindingsTransformerCreationInfos.getVoltageLevelId2());
@@ -1202,6 +1235,7 @@ public class NetworkModificationService {
                 } else {
                     addTwoWindingsTransformer(network, voltageLevel1, voltageLevel2, twoWindingsTransformerCreationInfos, true, true, subReporter);
                 }
+                nullDeadBandChecker.restore(twoWindingsTransformerCreationInfos);
             }
             // add the 2wt creation entity to the listener
             listener.storeTwoWindingsTransformerCreation(twoWindingsTransformerCreationInfos);
