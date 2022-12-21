f878da5a48246f186f908fd5e7bac3e7d8b683f4
David BRAQUART <david.braquart@rte-france.com>
2022-12-21 14:01:18 +0100

    review remarks

--- src/main/java/org/gridsuite/modification/server/NetworkModificationController.java
+++ src/main/java/org/gridsuite/modification/server/NetworkModificationController.java
@@ -62,7 +62,7 @@ public class NetworkModificationController {
     @PutMapping(value = "/groups/{groupUuid}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "For a list of network modifications passed in body, Move them before another one or at the end of the list, or Duplicate them at the end of the list")
     @ApiResponse(responseCode = "200", description = "The modification list of the group has been updated. Missing modifications are returned.")
-    public ResponseEntity<List<UUID>> updateModificationGroup(@Parameter(description = "updated group UUID, where modifications are pasted") @PathVariable("groupUuid") UUID groupUuid,
+    public ResponseEntity<List<UUID>> updateModificationGroup(@Parameter(description = "updated group UUID, where modifications are pasted") @PathVariable("groupUuid") UUID targetGroupUuid,
                                                               @Parameter(description = "kind of modification", required = true) @RequestParam(value = "action") GroupModificationAction action,
                                                               @Parameter(description = "the network uuid", required = true) @RequestParam(value = "networkUuid") UUID networkUuid,
                                                               @Parameter(description = "the report uuid", required = true) @RequestParam(value = "reportUuid") UUID reportUuid,
@@ -70,16 +70,18 @@ public class NetworkModificationController {
                                                               @Parameter(description = "the variant id", required = true) @RequestParam(value = "variantId") String variantId,
                                                               @Parameter(description = "the modification Uuid to move before (MOVE option, empty means moving at the end)") @RequestParam(value = "before", required = false) UUID before,
                                                               @Parameter(description = "origin group UUID, where modifications are copied or cut") @RequestParam(value = "originGroupUuid", required = false) UUID originGroupUuid,
-                                                              @Parameter(description = "destination node can be built (default is true)") @RequestParam(value = "build", required = false) Boolean build,
+                                                              @Parameter(description = "destination node can be built (default is true)") @RequestParam(value = "build", required = false, defaultValue = "true") Boolean build,
                                                               @RequestBody List<UUID> modificationsUuidList) {
         switch (action) {
             case COPY:
-                return ResponseEntity.ok().body(networkModificationService.duplicateModifications(groupUuid, networkUuid, reportUuid, reporterId, variantId, modificationsUuidList));
+                return ResponseEntity.ok().body(networkModificationService.duplicateModifications(targetGroupUuid, networkUuid, reportUuid, reporterId, variantId, modificationsUuidList));
             case MOVE:
-                // 2 params are optional
-                UUID sourceGroupUuid = originGroupUuid == null ? groupUuid : originGroupUuid;
-                boolean canBuildNode = build == null || build;
-                networkModificationService.moveModifications(groupUuid, sourceGroupUuid, before, networkUuid, reportUuid, reporterId, variantId, modificationsUuidList, canBuildNode);
+                UUID sourceGroupUuid = originGroupUuid == null ? targetGroupUuid : originGroupUuid;
+                boolean canBuildNode = build;
+                if (sourceGroupUuid.equals(originGroupUuid)) {
+                    canBuildNode = false;
+                }
+                networkModificationService.moveModifications(targetGroupUuid, sourceGroupUuid, before, networkUuid, reportUuid, reporterId, variantId, modificationsUuidList, canBuildNode);
                 return ResponseEntity.ok().body(List.of());
             default:
                 throw new NetworkModificationException(TYPE_MISMATCH);
