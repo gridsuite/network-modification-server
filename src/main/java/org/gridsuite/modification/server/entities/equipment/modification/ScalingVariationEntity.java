34d514f3098d3915d7f2880afb47aa148b7af68b
Seddik Yengui <seddik.yengui@rte-france.com>
2023-01-06 00:01:06 +0100

    fixes licences

--- src/main/java/org/gridsuite/modification/server/entities/equipment/modification/ScalingVariationEntity.java
+++ src/main/java/org/gridsuite/modification/server/entities/equipment/modification/ScalingVariationEntity.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -7,7 +7,6 @@
 
 package org.gridsuite.modification.server.entities.equipment.modification;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
