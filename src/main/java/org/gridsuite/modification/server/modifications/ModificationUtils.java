2d085e0952fb0aebef87c32efd223261b09bcb25
Etienne Homer <etiennehomer@gmail.com>
2023-04-13 13:03:10 +0200

    fix ;
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/modification/server/modifications/ModificationUtils.java
+++ src/main/java/org/gridsuite/modification/server/modifications/ModificationUtils.java
@@ -272,7 +272,7 @@ public final class ModificationUtils {
         voltageLevelTopologyBuilder.withVoltageLevelId(voltageLevelCreationInfos.getEquipmentId())
                 .withAlignedBusesOrBusbarCount(voltageLevelCreationInfos.getBusbarCount())
                 .withSectionCount(voltageLevelCreationInfos.getSectionCount())
-                .withSwitchKinds(voltageLevelCreationInfos.getSwitchKinds())
+                .withSwitchKinds(voltageLevelCreationInfos.getSwitchKinds());
 
         voltageLevelCreationInfos.getCouplingDevices().forEach(couplingDevice -> {
             CreateCouplingDeviceBuilder couplingDeviceBuilder = new CreateCouplingDeviceBuilder();
