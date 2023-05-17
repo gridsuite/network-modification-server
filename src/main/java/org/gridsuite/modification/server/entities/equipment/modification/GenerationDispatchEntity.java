9d89b8c79afb26cf073562714b67ba11e87bb223
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2023-05-17 16:02:20 +0200

    Generation dispatch : reducing generator maxP with forced and planned outage rate startup extension (#244)
    * Reducing generator maxP with forced and planned outage rate startup extension, and deal with forced maxP generators list
    
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/modification/server/entities/equipment/modification/GenerationDispatchEntity.java
+++ src/main/java/org/gridsuite/modification/server/entities/equipment/modification/GenerationDispatchEntity.java
@@ -11,13 +11,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.gridsuite.modification.server.dto.GenerationDispatchInfos;
+import org.gridsuite.modification.server.dto.GeneratorsWithoutOutageInfos;
 import org.gridsuite.modification.server.dto.ModificationInfos;
 import org.gridsuite.modification.server.entities.ModificationEntity;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
@@ -30,6 +35,13 @@ public class GenerationDispatchEntity extends ModificationEntity {
 
     @Column(name = "lossCoefficient")
     private double lossCoefficient;
+
+    @Column(name = "defaultOutageRate")
+    private double defaultOutageRate;
+
+    @ElementCollection
+    @CollectionTable(name = "generatorsWithoutOutage")
+    private List<GeneratorsWithoutOutageEmbeddable> generatorsWithoutOutage;
 
     public GenerationDispatchEntity(@NotNull GenerationDispatchInfos generationDispatchInfos) {
         super(generationDispatchInfos);
@@ -44,14 +56,29 @@ public class GenerationDispatchEntity extends ModificationEntity {
 
     private void assignAttributes(GenerationDispatchInfos generationDispatchInfos) {
         lossCoefficient = generationDispatchInfos.getLossCoefficient();
+        defaultOutageRate = generationDispatchInfos.getDefaultOutageRate();
+        generatorsWithoutOutage = toEmbeddable(generationDispatchInfos.getGeneratorsWithoutOutage());
+    }
+
+    public static List<GeneratorsWithoutOutageEmbeddable> toEmbeddable(List<GeneratorsWithoutOutageInfos> generators) {
+        return generators == null ? null : generators.stream()
+            .map(generator -> new GeneratorsWithoutOutageEmbeddable(generator.getId(), generator.getName()))
+            .collect(Collectors.toList());
     }
 
     @Override
     public GenerationDispatchInfos toModificationInfos() {
+        List<GeneratorsWithoutOutageInfos> generators = generatorsWithoutOutage != null ? generatorsWithoutOutage
+                .stream()
+                .map(generator -> new GeneratorsWithoutOutageInfos(generator.getId(), generator.getName()))
+                .collect(Collectors.toList()) : null;
+
         return GenerationDispatchInfos.builder()
                 .date(getDate())
                 .uuid(getId())
                 .lossCoefficient(getLossCoefficient())
+                .defaultOutageRate(getDefaultOutageRate())
+                .generatorsWithoutOutage(generators)
                 .build();
     }
 }
