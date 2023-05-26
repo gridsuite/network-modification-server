7c561c7dcf18a23002e59a259036759ffe1bfad8
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2023-05-26 11:06:33 +0200

    Eliminate duplicated generator id when exporting the filters. (#256)
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/modification/server/modifications/GenerationDispatch.java
+++ src/main/java/org/gridsuite/modification/server/modifications/GenerationDispatch.java
@@ -251,6 +251,7 @@ public class GenerationDispatch extends AbstractModification {
             .filter(f -> !filtersWithGeneratorsNotFound.containsKey(f.getFilterId()))
             .flatMap(f -> exportedGenerators.get(f.getFilterId()).getIdentifiableAttributes().stream())
             .map(IdentifiableAttributes::getId)
+            .distinct()
             .collect(Collectors.toList());
     }
 
