491081f28f743b452ded0e9ed90b31a6b60e576c
Hugo Marcellin <hugo.marcelin@rte-france.com>
2023-07-03 17:06:39 +0200

    Checkstyle

--- src/main/java/org/gridsuite/modification/server/dto/TapChangerModificationInfos.java
+++ src/main/java/org/gridsuite/modification/server/dto/TapChangerModificationInfos.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
