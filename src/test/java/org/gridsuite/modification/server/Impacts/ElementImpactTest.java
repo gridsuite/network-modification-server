057e450a3985096e65bcbdf5a868b92930d94b0c
Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>
2023-07-12 15:09:43 +0200

    Now use common lombok configuration (#281)
    Remove EqualsVerifier library used to cheat on coverage
    
    Signed-off-by: Tristan Chuine <tristan.chuine_externe@rte-france.com>

--- src/test/java/org/gridsuite/modification/server/Impacts/ElementImpactTest.java
+++ src/test/java/org/gridsuite/modification/server/Impacts/ElementImpactTest.java
@@ -8,7 +8,6 @@ package org.gridsuite.modification.server.Impacts;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.powsybl.iidm.network.IdentifiableType;
-import nl.jqno.equalsverifier.EqualsVerifier;
 import org.gridsuite.modification.server.dto.NetworkModificationResult;
 import org.gridsuite.modification.server.dto.NetworkModificationResult.ApplicationStatus;
 import org.gridsuite.modification.server.impacts.SimpleElementImpact;
@@ -21,6 +20,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.TreeSet;
+
 import static org.gridsuite.modification.server.Impacts.TestImpactUtils.*;
 import static org.junit.Assert.assertEquals;
 
@@ -33,9 +33,6 @@ public class ElementImpactTest {
 
     @Test
     public void testElementImpact() throws IOException {
-        EqualsVerifier.simple().forClass(NetworkModificationResult.class).verify();
-        EqualsVerifier.simple().forClass(SimpleElementImpact.class).verify();
-
         SimpleElementImpact creationImpact = createCreationImpactType(IdentifiableType.LINE, "lineId", new TreeSet<>(List.of("s1", "s2")));
         SimpleElementImpact modificationImpact = createModificationImpactType(IdentifiableType.LOAD, "loadId", new TreeSet<>(List.of("s3")));
         SimpleElementImpact deletionImpact = createDeletionImpactType(IdentifiableType.GENERATOR, "generatorId", new TreeSet<>(List.of("s4")));
