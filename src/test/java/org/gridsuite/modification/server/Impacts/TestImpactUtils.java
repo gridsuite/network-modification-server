1253cdda726fca02afc87e3e8cf0ddb44ce7e951
Ayoub LABIDI <ayoub.labidi@protonmail.com>
2023-07-31 10:05:17 +0200

    synchronize branch
    Signed-off-by: Ayoub LABIDI <ayoub.labidi@protonmail.com>

--- src/test/java/org/gridsuite/modification/server/Impacts/TestImpactUtils.java
+++ src/test/java/org/gridsuite/modification/server/Impacts/TestImpactUtils.java
@@ -127,7 +127,7 @@ public final class TestImpactUtils {
     public static void testBranchCreationImpacts(ObjectMapper mapper, String resultAsString, IdentifiableType elementType, String elementId, Set<String> substationIds) throws JsonProcessingException {
         List<SimpleElementImpact> impacts = List.of(
             createElementImpact(SimpleImpactType.CREATION, elementType, elementId, new TreeSet<>(substationIds)),
-            createElementImpact(SimpleImpactType.MODIFICATION, elementType, elementId, new TreeSet<>(substationIds)) // case with newCurrentLimits1
+            createElementImpact(SimpleImpactType.MODIFICATION, elementType, elementId, new TreeSet<>(substationIds)) // case with newCurrentLimits1/newtapChanger
         );
         testElementImpacts(mapper, resultAsString, impacts);
     }
@@ -145,10 +145,14 @@ public final class TestImpactUtils {
                                          String breakerId2, String disconnectorId2, String substationId2) throws JsonProcessingException {
         Optional<NetworkModificationResult> networkModificationResult = mapper.readValue(resultAsString, new TypeReference<>() { });
         assertTrue(networkModificationResult.isPresent());
+        List<SimpleElementImpact> impacts = createBranchImpacts(impactType, branchType, branchId, breakerId1, disconnectorId1, substationId1, breakerId2, disconnectorId2, substationId2);
+        if (impactType != SimpleImpactType.DELETION) {
+            impacts.add(createElementImpact(SimpleImpactType.MODIFICATION, branchType, branchId, new TreeSet<>(List.of(substationId1, substationId2)))); // case with newtapChanger
+        }
         NetworkModificationResult resultExpected = NetworkModificationResult.builder()
             .applicationStatus(ApplicationStatus.ALL_OK)
             .lastGroupApplicationStatus(ApplicationStatus.ALL_OK)
-            .networkImpacts(createBranchImpacts(impactType, branchType, branchId, breakerId1, disconnectorId1, substationId1, breakerId2, disconnectorId2, substationId2))
+            .networkImpacts(impacts)
             .build();
         assertThat(networkModificationResult.get()).recursivelyEquals(resultExpected);
     }
