281ad95a95c1768e00c27550423cfac27136ce5d
Ayoub LABIDI <ayoub.labidi@protonmail.com>
2023-08-09 18:28:05 +0200

    Fix tests
    Signed-off-by: Ayoub LABIDI <ayoub.labidi@protonmail.com>

--- src/test/java/org/gridsuite/modification/server/modifications/LineSplitWithNewVoltageLevelTest.java
+++ src/test/java/org/gridsuite/modification/server/modifications/LineSplitWithNewVoltageLevelTest.java
@@ -62,7 +62,7 @@ public class LineSplitWithNewVoltageLevelTest extends AbstractNetworkModificatio
                 .busbarCount(2)
                 .sectionCount(2)
                 .switchKinds(Arrays.asList(SwitchKind.BREAKER))
-                .couplingDevices(Arrays.asList(CouplingDeviceInfos.builder().busbarSectionId1("bbs.nw").busbarSectionId2("bbs.ne").build()))
+                .couplingDevices(Arrays.asList(CouplingDeviceInfos.builder().busbarSectionId1("1A").busbarSectionId2("1B").build()))
                 .build();
 
         return LineSplitWithVoltageLevelInfos.builder()
