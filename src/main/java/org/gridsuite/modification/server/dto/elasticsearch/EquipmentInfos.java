7f82d33b7adbfa8d2bf17c633e7eb546491405b0
achour94 <berrahmaachour@gmail.com>
2023-08-31 16:29:03 +0200

    add substations to EquipmentInfos

--- src/main/java/org/gridsuite/modification/server/dto/elasticsearch/EquipmentInfos.java
+++ src/main/java/org/gridsuite/modification/server/dto/elasticsearch/EquipmentInfos.java
@@ -10,6 +10,7 @@ import com.powsybl.iidm.network.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.gridsuite.modification.server.NetworkModificationException;
+import org.gridsuite.modification.server.dto.SubstationInfos;
 import org.gridsuite.modification.server.dto.VoltageLevelInfos;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.elasticsearch.annotations.*;
@@ -48,6 +49,9 @@ public class EquipmentInfos extends BasicEquipmentInfos {
     @Field(type = FieldType.Nested, includeInParent = true)
     private Set<VoltageLevelInfos> voltageLevels;
 
+    @Field(type = FieldType.Nested, includeInParent = true)
+    private Set<SubstationInfos> substations;
+
     public static Set<VoltageLevelInfos> getVoltageLevels(@NonNull Identifiable<?> identifiable) {
         if (identifiable instanceof Substation) {
             return ((Substation) identifiable).getVoltageLevelStream().map(vl -> VoltageLevelInfos.builder().id(vl.getId()).name(vl.getNameOrId()).build()).collect(Collectors.toSet());
@@ -84,4 +88,45 @@ public class EquipmentInfos extends BasicEquipmentInfos {
 
         throw NetworkModificationException.createEquipmentTypeUnknown(identifiable.getClass().getSimpleName());
     }
+
+    public static Set<SubstationInfos> getSubstations(@NonNull Identifiable<?> identifiable) {
+        if (identifiable instanceof Substation substation) {
+            return Set.of(SubstationInfos.builder().id(substation.getId()).name(substation.getNameOrId()).build());
+        } else if (identifiable instanceof VoltageLevel voltageLevel) {
+            return Set.of(SubstationInfos.builder().id(voltageLevel.getSubstation().map(Substation::getId).orElse(null)).name(voltageLevel.getSubstation().map(Substation::getNameOrId).orElse(null)).build());
+        } else if (identifiable instanceof Switch switchEquipment) {
+            return Set.of(SubstationInfos.builder().id(switchEquipment.getVoltageLevel().getSubstation().map(Substation::getId).orElse(null)).name(switchEquipment.getVoltageLevel().getSubstation().map(Substation::getNameOrId).orElse(null)).build());
+        } else if (identifiable instanceof Injection<?> injection) {
+            return Set.of(SubstationInfos.builder().id(injection.getTerminal().getVoltageLevel().getSubstation().map(Substation::getId).orElse(null)).name(injection.getTerminal().getVoltageLevel().getSubstation().map(Substation::getNameOrId).orElse(null)).build());
+        } else if (identifiable instanceof Bus bus) {
+            return Set.of(SubstationInfos.builder().id(bus.getVoltageLevel().getSubstation().map(Substation::getId).orElse(null)).name(bus.getVoltageLevel().getSubstation().map(Substation::getNameOrId).orElse(null)).build());
+        } else if (identifiable instanceof HvdcLine hvdcLine) {
+            Substation substation1 = hvdcLine.getConverterStation1().getTerminal().getVoltageLevel().getSubstation().orElse(null);
+            Substation substation2 = hvdcLine.getConverterStation2().getTerminal().getVoltageLevel().getSubstation().orElse(null);
+            return Stream.of(
+                    SubstationInfos.builder().id(substation1 != null ? substation1.getId() : null).name(substation1 != null ? substation1.getNameOrId() : null).build(),
+                    SubstationInfos.builder().id(substation2 != null ? substation2.getId() : null).name(substation2 != null ? substation2.getNameOrId() : null).build()
+                )
+                .collect(Collectors.toSet());
+        } else if (identifiable instanceof Branch branch) {
+            Substation substation1 = branch.getTerminal1().getVoltageLevel().getSubstation().orElse(null);
+            Substation substation2 = branch.getTerminal2().getVoltageLevel().getSubstation().orElse(null);
+            return Stream.of(
+                            SubstationInfos.builder().id(substation1 != null ? substation1.getId() : null).name(substation1 != null ? substation1.getNameOrId() : null).build(),
+                            SubstationInfos.builder().id(substation2 != null ? substation2.getId() : null).name(substation2 != null ? substation2.getNameOrId() : null).build()
+                    )
+                    .collect(Collectors.toSet());
+        } else if (identifiable instanceof ThreeWindingsTransformer w3t) {
+            Substation substation1 = w3t.getTerminal(ThreeWindingsTransformer.Side.ONE).getVoltageLevel().getSubstation().orElse(null);
+            Substation substation2 = w3t.getTerminal(ThreeWindingsTransformer.Side.TWO).getVoltageLevel().getSubstation().orElse(null);
+            Substation substation3 = w3t.getTerminal(ThreeWindingsTransformer.Side.THREE).getVoltageLevel().getSubstation().orElse(null);
+            return Stream.of(
+                SubstationInfos.builder().id(substation1 != null ? substation1.getId() : null).name(substation1 != null ? substation1.getNameOrId() : null).build(),
+                SubstationInfos.builder().id(substation2 != null ? substation2.getId() : null).name(substation2 != null ? substation2.getNameOrId() : null).build(),
+                SubstationInfos.builder().id(substation3 != null ? substation3.getId() : null).name(substation3 != null ? substation3.getNameOrId() : null).build()
+            ).collect(Collectors.toSet());
+        }
+        throw NetworkModificationException.createEquipmentTypeUnknown(identifiable.getClass().getSimpleName());
+    }
+
 }
