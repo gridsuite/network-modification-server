1f9f70e8d4bc971c0eaa7132e210884b9c31daf5
Walid-Sahnoun <107688384+Walid-Sahnoun@users.noreply.github.com>
2023-09-20 15:31:33 +0200

    delete_stashed_modifications_when_stash_node_or_subtree (#331)
    delete_stashed_modification_when_stash_node_or_subtree
    Signed-off-by: walid-sahnoun <walid.sahnoun@rte-france.com>

--- src/main/java/org/gridsuite/modification/server/service/NetworkModificationService.java
+++ src/main/java/org/gridsuite/modification/server/service/NetworkModificationService.java
@@ -289,4 +289,8 @@ public class NetworkModificationService {
         List<ModificationEntity> duplicatedModificationsEntities = networkModificationRepository.copyModificationsEntities(originGroupUuid);
         return saveAndApplyModifications(targetGroupUuid, networkUuid, variantId, reportInfos, duplicatedModificationsEntities);
     }
+
+    public void deleteStashedModificationInGroup(UUID groupUuid, boolean errorOnGroupNotFound) {
+        networkModificationRepository.deleteStashedModificationInGroup(groupUuid, errorOnGroupNotFound);
+    }
 }
