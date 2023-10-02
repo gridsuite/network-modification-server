bc2601766efb668f65267022631ec56c9535a45e
Etienne Homer <etiennehomer@gmail.com>
2023-10-02 18:13:44 +0200

    tmp2
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/modification/server/entities/TabularModificationEntity.java
+++ src/main/java/org/gridsuite/modification/server/entities/TabularModificationEntity.java
@@ -7,6 +7,12 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.gridsuite.modification.server.dto.ModificationInfos;
+import org.gridsuite.modification.server.dto.TabularModificationInfos;
+import org.gridsuite.modification.server.dto.VoltageInitGeneratorModificationInfos;
+import org.gridsuite.modification.server.entities.equipment.modification.VoltageInitGeneratorModificationEmbeddable;
 
 @NoArgsConstructor
 @Getter
@@ -24,6 +30,12 @@ public class TabularModificationEntity extends ModificationEntity {
 
     public TabularModificationEntity(TabularModificationInfos tabularModificationInfos) {
         super(tabularModificationInfos);
-        modifications = tabularModificationInfos.getModifications;
+        modifications = toEmbeddableVoltageInitGenerators(tabularModificationInfos.getGenerators());
+    }
+
+    public static List<VoltageInitGeneratorModificationEmbeddable> toEmbeddableVoltageInitGenerators(List<ModificationInfos> generators) {
+        return generators == null ? null : generators.stream()
+                .map(generator -> new VoltageInitGeneratorModificationEmbeddable(generator.getGeneratorId(), generator.getVoltageSetpoint(), generator.getReactivePowerSetpoint()))
+                .collect(Collectors.toList());
     }
 }
