56074a0c3c1d145c945c2092e3b5ebad3c69cea9
Seddik Yengui <seddik.yengui@rte-france.com>
2023-11-10 14:05:29 +0100

    fix

--- src/main/java/org/gridsuite/modification/server/modifications/ByFormulaModification.java
+++ src/main/java/org/gridsuite/modification/server/modifications/ByFormulaModification.java
@@ -175,7 +175,7 @@ public class ByFormulaModification extends AbstractModification {
                 case DIVISION -> {
                     if (value2 == 0) {
                         throw new NetworkModificationException(NetworkModificationException.Type.BY_FORMULA_MODIFICATION_ERROR,
-                                "there is a division by 0 in one formula");
+                                "there is a division by zero in one formula");
                     } else {
                         yield value1 / value2;
                     }
