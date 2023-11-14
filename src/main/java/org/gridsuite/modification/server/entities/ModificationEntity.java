0a320d8fec276efbee619831478c11a2a5f7f3bc
sBouzols <sylvain.bouzols@gmail.com>
2023-11-14 12:37:19 +0100

    refactor(): stashed status management cleanup
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/main/java/org/gridsuite/modification/server/entities/ModificationEntity.java
+++ src/main/java/org/gridsuite/modification/server/entities/ModificationEntity.java
@@ -49,8 +49,8 @@ public class ModificationEntity {
     @Setter
     private ModificationGroupEntity group;
 
-    @Column(name = "stashed")
-    private Boolean stashed;
+    @Column(name = "stashed", columnDefinition = "boolean default false")
+    private Boolean stashed = false;
 
     @Column(name = "modifications_order")
     private int modificationsOrder;
@@ -76,7 +76,6 @@ public class ModificationEntity {
         }
         //We need to limit the precision to avoid database precision storage limit issue (postgres has a precision of 6 digits while h2 can go to 9)
         this.date = ZonedDateTime.now(ZoneOffset.UTC).truncatedTo(ChronoUnit.MICROS);
-        this.stashed = modificationInfos.getStashed();
         assignAttributes(modificationInfos);
     }
 
@@ -97,6 +96,7 @@ public class ModificationEntity {
         this.setType(modificationInfos.getType().name());
         this.setMessageType(modificationInfos.getType().name());
         this.setMessageValues(new ObjectMapper().writeValueAsString(modificationInfos.getMapMessageValues()));
+        this.setStashed(modificationInfos.getStashed());
     }
 
     public ModificationEntity copy() {
