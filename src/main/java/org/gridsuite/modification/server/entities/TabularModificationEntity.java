6294e79d62a8e13fb24d649d8782832b5728d723
Etienne Homer <etiennehomer@gmail.com>
2023-12-18 12:30:15 +0100

    Remove hashCode and equals override
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/modification/server/entities/TabularModificationEntity.java
+++ src/main/java/org/gridsuite/modification/server/entities/TabularModificationEntity.java
@@ -81,13 +81,4 @@ public class TabularModificationEntity extends ModificationEntity {
         }
     }
 
-    @Override
-    public boolean equals(Object o) {
-        return super.equals(o);
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
 }
