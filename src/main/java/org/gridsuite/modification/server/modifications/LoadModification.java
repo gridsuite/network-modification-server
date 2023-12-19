f797e57ae74c97d2510d4c5dc5a108634dea932a
Lilian Houdelet <lhoudelet@oxyl.fr>
2023-12-19 14:15:43 +0100

    Fix from comments
    Signed-off-by: Lilian Houdelet <lhoudelet@oxyl.fr>

--- src/main/java/org/gridsuite/modification/server/modifications/LoadModification.java
+++ src/main/java/org/gridsuite/modification/server/modifications/LoadModification.java
@@ -46,8 +46,8 @@ public class LoadModification extends AbstractModification {
 
         ModificationUtils.getInstance().applyElementaryModifications(load::setName, () -> load.getOptionalName().orElse("No value"), loadModificationInfos.getEquipmentName(), subReporter, "Name");
         ModificationUtils.getInstance().applyElementaryModifications(load::setLoadType, load::getLoadType, loadModificationInfos.getLoadType(), subReporter, "Type");
-        ModificationUtils.getInstance().applyElementaryModifications(load::setP0, load::getP0, loadModificationInfos.getConstantActivePower(), subReporter, "Active power");
-        ModificationUtils.getInstance().applyElementaryModifications(load::setQ0, load::getQ0, loadModificationInfos.getConstantReactivePower(), subReporter, "Reactive power");
+        ModificationUtils.getInstance().applyElementaryModifications(load::setP0, load::getP0, loadModificationInfos.getConstantActivePower(), subReporter, "Constant active power");
+        ModificationUtils.getInstance().applyElementaryModifications(load::setQ0, load::getQ0, loadModificationInfos.getConstantReactivePower(), subReporter, "Constant reactive power");
 
         // TODO connectivity modification
     }
