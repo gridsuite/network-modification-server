8f4fc2321d27ef035e82bb25abe861901fad1fc1
David SARTORI <david.sartori_externe@rte-france.com>
2023-12-28 18:26:34 +0100

    Prevent tabular modification for non-linear shunt compensators

--- src/main/java/org/gridsuite/modification/server/modifications/TabularModification.java
+++ src/main/java/org/gridsuite/modification/server/modifications/TabularModification.java
@@ -11,13 +11,12 @@ import com.powsybl.commons.reporter.Report;
 import com.powsybl.commons.reporter.Reporter;
 import com.powsybl.commons.reporter.TypedValue;
 import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.ShuntCompensatorModelType;
 import org.gridsuite.modification.server.NetworkModificationException;
 import org.gridsuite.modification.server.dto.ShuntCompensatorModificationInfos;
 import org.gridsuite.modification.server.dto.TabularModificationInfos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.gridsuite.modification.server.NetworkModificationException.Type.TABULAR_MODIFICATION_ERROR;
 
@@ -60,22 +59,34 @@ public class TabularModification extends AbstractModification {
 
     @Override
     public void apply(Network network, Reporter subReporter) {
-        AtomicInteger applicationFailuresCount = new AtomicInteger(0);
-        modificationInfos.getModifications().forEach(modifInfos -> {
+        int applicationFailuresCount = 0;
+        for (var modifInfos : modificationInfos.getModifications()) {
+            if (modifInfos instanceof ShuntCompensatorModificationInfos shuntModifInfos) {
+                var shuntCompensator = network.getShuntCompensator(shuntModifInfos.getEquipmentId());
+                if (shuntCompensator.getModelType() == ShuntCompensatorModelType.NON_LINEAR) {
+                    applicationFailuresCount++;
+                    subReporter.report(Report.builder()
+                            .withKey(shuntCompensator.getId())
+                            .withDefaultMessage("Tabular modification: It is currently not possible to modify non-linear shunt compensator with id " + shuntCompensator.getId())
+                            .withSeverity(TypedValue.ERROR_SEVERITY)
+                            .build());
+                    continue;
+                }
+            }
             try {
                 AbstractModification modification = modifInfos.toModification();
                 modification.check(network);
                 modification.apply(network);
             } catch (PowsyblException e) {
-                applicationFailuresCount.incrementAndGet();
+                applicationFailuresCount++;
                 subReporter.report(Report.builder()
-                        .withKey(modifInfos.getType().name() + applicationFailuresCount.get())
+                        .withKey(modifInfos.getType().name() + applicationFailuresCount)
                         .withDefaultMessage(e.getMessage())
                         .withSeverity(TypedValue.WARN_SEVERITY)
                         .build());
                 LOGGER.warn(e.getMessage());
             }
-        });
+        }
         String defaultMessage = " have been modified";
         defaultMessage = switch (modificationInfos.getModificationType()) {
             case "GENERATOR_MODIFICATION" -> "generators" + defaultMessage;
@@ -88,18 +99,18 @@ public class TabularModification extends AbstractModification {
             default -> "equipments of unknown type" + defaultMessage;
         };
 
-        if (modificationInfos.getModifications().size() == applicationFailuresCount.get()) {
+        if (modificationInfos.getModifications().size() == applicationFailuresCount) {
             subReporter.report(Report.builder()
                     .withKey(TABULAR_MODIFICATION_REPORT_KEY + "Error")
                     .withDefaultMessage("Tabular modification: No " + defaultMessage)
                     .withSeverity(TypedValue.ERROR_SEVERITY)
                     .build());
-        } else if (applicationFailuresCount.get() > 0) {
+        } else if (applicationFailuresCount > 0) {
             subReporter.report(Report.builder()
                     .withKey(TABULAR_MODIFICATION_REPORT_KEY + "Warning")
                     .withDefaultMessage("Tabular modification: ${modificationsCount} " + defaultMessage + " and ${failuresCount} have not been modified")
-                    .withValue("modificationsCount", modificationInfos.getModifications().size() - applicationFailuresCount.get())
-                    .withValue("failuresCount", applicationFailuresCount.get())
+                    .withValue("modificationsCount", modificationInfos.getModifications().size() - applicationFailuresCount)
+                    .withValue("failuresCount", applicationFailuresCount)
                     .withSeverity(TypedValue.WARN_SEVERITY)
                     .build());
         } else {
