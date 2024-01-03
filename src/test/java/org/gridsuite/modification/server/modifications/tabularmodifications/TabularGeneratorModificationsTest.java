e909f4c3de1a7fcc50aba83e8caab92cbe6ae492
Florent MILLOT <millotflo@gmail.com>
2024-01-03 11:36:23 +0100

    Update comment
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- src/test/java/org/gridsuite/modification/server/modifications/tabularmodifications/TabularGeneratorModificationsTest.java
+++ src/test/java/org/gridsuite/modification/server/modifications/tabularmodifications/TabularGeneratorModificationsTest.java
@@ -122,7 +122,7 @@ public class TabularGeneratorModificationsTest extends AbstractNetworkModificati
         assertSelectCount(3);
         reset();
 
-        // Now we get back the 2 tabular modifications
+        // We get the modifications of the group (so the 2 tabular modifications)
         mockMvc.perform(get("/v1/groups/{groupUuid}/network-modifications", getGroupId()))
                 .andExpect(status().isOk());
         // We check that the request count is not dependent on the number of sub modifications of the tabular modification (the JPA N+1 problem is correctly solved)
