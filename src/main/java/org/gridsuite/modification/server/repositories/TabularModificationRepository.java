b7846eb4e10e212d460554fdc58f30cadaaa74a4
Joris Mancini <joris.mancini_externe@rte-france.com>
2024-02-06 12:22:33 +0100

    chore: copyright and author
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- src/main/java/org/gridsuite/modification/server/repositories/TabularModificationRepository.java
+++ src/main/java/org/gridsuite/modification/server/repositories/TabularModificationRepository.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.modification.server.repositories;
 
 import org.gridsuite.modification.server.ModificationType;
@@ -10,6 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * @author Joris Mancini <joris.mancini_externe at rte-france.com>
+ */
 @Repository
 public class TabularModificationRepository {
 
@@ -52,7 +61,7 @@ public class TabularModificationRepository {
         this.voltageLevelModificationRepository = voltageLevelModificationRepository;
     }
 
-    @Transactional // To have the 2 delete in the same transaction (atomic)
+    @Transactional
     public void deleteAll() {
         batteryModificationRepository.deleteAll();
         generatorModificationRepository.deleteAll();
