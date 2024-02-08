a0f40793f4e87759a7da419732b733743b1e96c3
Franck LECUYER <franck.lecuyer@rte-france.com>
2024-02-08 15:56:15 +0100

    Add generators tabular creations
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

---
+++ src/main/java/org/gridsuite/modification/server/dto/TabularCreationInfos.java
@@ -0,0 +1,71 @@
+/*
+  Copyright (c) 2024, RTE (http://www.rte-france.com)
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.modification.server.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.powsybl.commons.reporter.Reporter;
+import com.powsybl.commons.reporter.ReporterModel;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.gridsuite.modification.server.ModificationType;
+import org.gridsuite.modification.server.dto.annotation.ModificationErrorTypeName;
+import org.gridsuite.modification.server.entities.ModificationEntity;
+import org.gridsuite.modification.server.entities.TabularCreationEntity;
+import org.gridsuite.modification.server.modifications.AbstractModification;
+import org.gridsuite.modification.server.modifications.TabularCreation;
+import org.springframework.lang.NonNull;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
+ */
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@Data
+@Schema(description = "Tabular creation")
+@JsonTypeName("TABULAR_CREATION")
+@ModificationErrorTypeName("TABULAR_CREATION_ERROR")
+public class TabularCreationInfos extends ModificationInfos {
+
+    @Schema(description = "Creation type")
+    @NonNull
+    private ModificationType creationType;
+
+    @Schema(description = "creations")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<ModificationInfos> creations;
+
+    @Override
+    public ModificationEntity toEntity() {
+        return new TabularCreationEntity(this);
+    }
+
+    @Override
+    public AbstractModification toModification() {
+        return new TabularCreation(this);
+    }
+
+    @Override
+    public Reporter createSubReporter(ReporterModel reporter) {
+        return reporter.createSubReporter(ModificationType.TABULAR_CREATION.name(), "Tabular creation");
+    }
+
+    @Override
+    public Map<String, String> getMapMessageValues() {
+        Map<String, String> mapMessageValues = new HashMap<>();
+        mapMessageValues.put("tabularCreationType", getCreationType().name());
+        return mapMessageValues;
+    }
+}
