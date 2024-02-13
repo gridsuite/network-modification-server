596b91d3df058256e5a9b2cba9040a2e5b8369e6
Slimane AMAR <amarsli@gm0winl104.bureau.si.interne>
2024-02-13 16:27:18 +0100

    Review remarks
    Signed-off-by: Slimane AMAR <amarsli@gm0winl104.bureau.si.interne>

--- src/main/java/org/gridsuite/modification/server/modifications/OperatingStatusModification.java
+++ src/main/java/org/gridsuite/modification/server/modifications/OperatingStatusModification.java
@@ -9,7 +9,8 @@ package org.gridsuite.modification.server.modifications;
 import com.powsybl.commons.reporter.Report;
 import com.powsybl.commons.reporter.Reporter;
 import com.powsybl.commons.reporter.TypedValue;
-import com.powsybl.iidm.modification.tripping.*;
+import com.powsybl.iidm.modification.tripping.BranchTripping;
+import com.powsybl.iidm.modification.tripping.Tripping;
 import com.powsybl.iidm.network.*;
 import com.powsybl.iidm.network.extensions.OperatingStatus;
 import com.powsybl.iidm.network.extensions.OperatingStatusAdder;
@@ -22,7 +23,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static org.gridsuite.modification.server.NetworkModificationException.Type.*;
+import static org.gridsuite.modification.server.NetworkModificationException.Type.EQUIPMENT_NOT_FOUND;
+import static org.gridsuite.modification.server.NetworkModificationException.Type.OPERATING_STATUS_MODIFICATION_ERROR;
 import static org.gridsuite.modification.server.modifications.ModificationUtils.distinctByKey;
 
 /**
@@ -113,41 +115,43 @@ public class OperatingStatusModification extends AbstractModification {
     }
 
     private void applySwitchOnEquipment(Reporter subReporter, Identifiable<?> equipment, String equipmentType) {
-        if (equipment instanceof Branch<?>) {
-            if (connectAllTerminals(equipment)) {
-                equipment.newExtension(OperatingStatusAdder.class).withStatus(OperatingStatus.Status.IN_OPERATION).add();
-            } else {
-                throw new NetworkModificationException(OPERATING_STATUS_MODIFICATION_ERROR, "Unable to connect all equipment ends");
-            }
-
-            subReporter.report(Report.builder()
-                    .withKey("switchOn" + equipmentType + APPLIED)
-                    .withDefaultMessage(equipmentType + " ${id} (id) : switch on applied")
-                    .withValue("id", equipment.getId())
-                    .withSeverity(TypedValue.INFO_SEVERITY)
-                    .build());
+        if (!(equipment instanceof Branch<?>)) {
+            throw NetworkModificationException.createEquipmentTypeNotSupported(equipment.getClass().getSimpleName());
         }
-        throw NetworkModificationException.createEquipmentTypeNotSupported(equipment.getClass().getSimpleName());
+
+        if (connectAllTerminals(equipment)) {
+            equipment.newExtension(OperatingStatusAdder.class).withStatus(OperatingStatus.Status.IN_OPERATION).add();
+        } else {
+            throw new NetworkModificationException(OPERATING_STATUS_MODIFICATION_ERROR, "Unable to connect all equipment ends");
+        }
+
+        subReporter.report(Report.builder()
+                .withKey("switchOn" + equipmentType + APPLIED)
+                .withDefaultMessage(equipmentType + " ${id} (id) : switch on applied")
+                .withValue("id", equipment.getId())
+                .withSeverity(TypedValue.INFO_SEVERITY)
+                .build());
     }
 
     private void applyEnergiseEquipmentEnd(Reporter subReporter, Identifiable<?> equipment, String equipmentType, TwoSides side) {
-        if (equipment instanceof Branch<?> branch) {
-            TwoSides oppositeSide = side == TwoSides.ONE ? TwoSides.TWO : TwoSides.ONE;
-            if (connectOneTerminal(branch.getTerminal(side)) && disconnectOneTerminal(branch.getTerminal(oppositeSide))) {
-                branch.newExtension(OperatingStatusAdder.class).withStatus(OperatingStatus.Status.IN_OPERATION).add();
-            } else {
-                throw new NetworkModificationException(OPERATING_STATUS_MODIFICATION_ERROR, "Unable to energise equipment end");
-            }
-
-            subReporter.report(Report.builder()
-                    .withKey("energise" + equipmentType + "EndApplied")
-                    .withDefaultMessage(equipmentType + " ${id} (id) : energise the side ${side} applied")
-                    .withValue("id", equipment.getId())
-                    .withValue("side", side.name())
-                    .withSeverity(TypedValue.INFO_SEVERITY)
-                    .build());
+        if (!(equipment instanceof Branch<?> branch)) {
+            throw NetworkModificationException.createEquipmentTypeNotSupported(equipment.getClass().getSimpleName());
         }
-        throw NetworkModificationException.createEquipmentTypeNotSupported(equipment.getClass().getSimpleName());
+
+        TwoSides oppositeSide = side == TwoSides.ONE ? TwoSides.TWO : TwoSides.ONE;
+        if (connectOneTerminal(branch.getTerminal(side)) && disconnectOneTerminal(branch.getTerminal(oppositeSide))) {
+            branch.newExtension(OperatingStatusAdder.class).withStatus(OperatingStatus.Status.IN_OPERATION).add();
+        } else {
+            throw new NetworkModificationException(OPERATING_STATUS_MODIFICATION_ERROR, "Unable to energise equipment end");
+        }
+
+        subReporter.report(Report.builder()
+                .withKey("energise" + equipmentType + "EndApplied")
+                .withDefaultMessage(equipmentType + " ${id} (id) : energise the side ${side} applied")
+                .withValue("id", equipment.getId())
+                .withValue("side", side.name())
+                .withSeverity(TypedValue.INFO_SEVERITY)
+                .build());
     }
 
     private boolean disconnectAllTerminals(Identifiable<?> equipment) {
