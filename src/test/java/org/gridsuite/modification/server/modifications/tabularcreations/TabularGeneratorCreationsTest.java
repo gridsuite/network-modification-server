3df5fd7707303097c38250591a3783f55885c2a5
maissa SOUISSI <maissa.souissi@rte-france.com>
2024-03-05 17:37:04 +0100

    test
    Signed-off-by: maissa SOUISSI <maissa.souissi@rte-france.com>

--- src/test/java/org/gridsuite/modification/server/modifications/tabularcreations/TabularGeneratorCreationsTest.java
+++ src/test/java/org/gridsuite/modification/server/modifications/tabularcreations/TabularGeneratorCreationsTest.java
@@ -105,6 +105,10 @@ public class TabularGeneratorCreationsTest extends AbstractNetworkModificationTe
             .build();
     }
 
+
+
+
+
     @Override
     protected ModificationInfos buildModificationUpdate() {
         List<ModificationInfos> creations = List.of(
