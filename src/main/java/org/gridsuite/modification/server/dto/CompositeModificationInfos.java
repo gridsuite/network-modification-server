1bf32181c937562025fbfc1342b6be967edf7f5a
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-06-06 19:25:09 +0200

    add test

--- src/main/java/org/gridsuite/modification/server/dto/CompositeModificationInfos.java
+++ src/main/java/org/gridsuite/modification/server/dto/CompositeModificationInfos.java
@@ -8,6 +8,7 @@ package org.gridsuite.modification.server.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.powsybl.commons.report.ReportNode;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -46,6 +47,13 @@ public class CompositeModificationInfos extends ModificationInfos {
     @Override
     public AbstractModification toModification() {
         return new CompositeModification(this);
+    }
+
+    @Override
+    public ReportNode createSubReportNode(ReportNode reportNode) {
+        return reportNode.newReportNode()
+                .withMessageTemplate(ModificationType.COMPOSITE_MODIFICATION.name(), "Composite modification")
+                .add();
     }
 
     @Override
---
+++ src/test/java/org/gridsuite/modification/server/modifications/CompositeModificationsTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.gridsuite.modification.server.modifications;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.powsybl.iidm.network.Network;
+import lombok.SneakyThrows;
+import org.gridsuite.modification.server.ModificationType;
+import org.gridsuite.modification.server.dto.*;
+import org.gridsuite.modification.server.utils.NetworkCreation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Ghazwa Rehili <ghazwa.rehili at rte-france.com>
+ */
+@Tag("IntegrationTest")
+public class CompositeModificationsTest extends AbstractNetworkModificationTest {
+    @Override
+    protected Network createNetwork(UUID networkUuid) {
+        return NetworkCreation.create(networkUuid, true);
+    }
+
+    @Override
+    protected ModificationInfos buildModification() {
+        List<ModificationInfos> modifications = List.of(
+                LoadModificationInfos.builder().equipmentId("v1load").q0(new AttributeModification<>(300., OperationType.SET)).build(),
+                LoadModificationInfos.builder().equipmentId("v2load").q0(new AttributeModification<>(300., OperationType.SET)).build(),
+                LoadModificationInfos.builder().equipmentId("v3load").q0(new AttributeModification<>(300., OperationType.SET)).build()
+        );
+        return CompositeModificationInfos.builder()
+                .compositeModificationsList(modifications)
+                .stashed(false)
+                .build();
+    }
+
+    @Override
+    protected ModificationInfos buildModificationUpdate() {
+        List<ModificationInfos> modifications = List.of(
+                LoadModificationInfos.builder().equipmentId("v1load").q0(new AttributeModification<>(300., OperationType.SET)).build(),
+                LoadModificationInfos.builder().equipmentId("v2load").q0(new AttributeModification<>(300., OperationType.SET)).build(),
+                LoadModificationInfos.builder().equipmentId("v3load").q0(new AttributeModification<>(300., OperationType.SET)).build()
+        );
+        return CompositeModificationInfos.builder()
+                .compositeModificationsList(modifications)
+                .stashed(false)
+                .build();
+    }
+
+    @Override
+    protected void assertAfterNetworkModificationCreation() {
+        assertEquals(300., getNetwork().getLoad("v1load").getQ0(), 0.001);
+        assertEquals(300., getNetwork().getLoad("v2load").getQ0(), 0.001);
+        assertEquals(300., getNetwork().getLoad("v3load").getQ0(), 0.001);
+    }
+
+    @Override
+    protected void assertAfterNetworkModificationDeletion() {
+        assertEquals(0., getNetwork().getLoad("v1load").getQ0(), 0.001);
+        assertEquals(0., getNetwork().getLoad("v2load").getQ0(), 0.001);
+        assertEquals(0., getNetwork().getLoad("v3load").getQ0(), 0.001);
+    }
+
+    @Override
+    @SneakyThrows
+    protected void testCreationModificationMessage(ModificationInfos modificationInfos) {
+        assertEquals(ModificationType.COMPOSITE_MODIFICATION.name(), modificationInfos.getMessageType());
+        Map<String, String> createdValues = mapper.readValue(modificationInfos.getMessageValues(), new TypeReference<>() { });
+        Assertions.assertEquals(ModificationType.COMPOSITE_MODIFICATION.name(), createdValues.get("compositeModificationType"));
+    }
+
+    @Override
+    @SneakyThrows
+    protected void testUpdateModificationMessage(ModificationInfos modificationInfos) {
+        assertEquals(ModificationType.COMPOSITE_MODIFICATION.name(), modificationInfos.getMessageType());
+        Map<String, String> updatedValues = mapper.readValue(modificationInfos.getMessageValues(), new TypeReference<>() { });
+        Assertions.assertEquals(ModificationType.COMPOSITE_MODIFICATION.name(), updatedValues.get("compositeModificationType"));
+    }
+}
