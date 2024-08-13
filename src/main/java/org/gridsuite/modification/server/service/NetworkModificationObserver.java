57c9bde9655357d1cfba043b3fd75b8969b9d4d3
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2024-08-13 10:06:52 +0200

    feat: add observer to expose modification apply metrics
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

---
+++ src/main/java/org/gridsuite/modification/server/service/NetworkModificationObserver.java
@@ -0,0 +1,29 @@
+package org.gridsuite.modification.server.service;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.NonNull;
+import org.gridsuite.modification.server.ModificationType;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NetworkModificationObserver {
+    protected static final String OBSERVATION_PREFIX = "app.network-modification.";
+    protected static final String MODIFICATION_TYPE_TAG_NAME = "modification_type";
+
+    private final ObservationRegistry observationRegistry;
+
+    public NetworkModificationObserver(@NonNull ObservationRegistry observationRegistry) {
+        this.observationRegistry = observationRegistry;
+    }
+
+    public <E extends Throwable> void observe(String name, ModificationType modificationType, Observation.CheckedRunnable<E> runnable) throws E {
+        createObservation(name, modificationType).observeChecked(runnable);
+    }
+
+    protected Observation createObservation(String name, ModificationType modificationType) {
+        return Observation.createNotStarted(OBSERVATION_PREFIX + name, observationRegistry)
+            .lowCardinalityKeyValue(MODIFICATION_TYPE_TAG_NAME, modificationType.name());
+    }
+
+}
