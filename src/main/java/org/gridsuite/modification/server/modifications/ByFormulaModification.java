2ce33edbdd60ba155e254085d2d61c60af761770
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2024-09-03 13:24:48 +0200

    always send formulaReports
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/main/java/org/gridsuite/modification/server/modifications/ByFormulaModification.java
+++ src/main/java/org/gridsuite/modification/server/modifications/ByFormulaModification.java
@@ -98,7 +98,6 @@ public class ByFormulaModification extends AbstractModification {
                         .withUntypedValue(KEY_VALUE, equipmentCount)
                         .withSeverity(TypedValue.INFO_SEVERITY)
                         .add();
-                report(formulaSubReporter, formulaReports);
             } else {
                 if (equipmentNotModifiedCount == equipmentCount) {
                     createReport(subReportNode, "byFormulaModificationNone",
@@ -111,9 +110,9 @@ public class ByFormulaModification extends AbstractModification {
                             .withUntypedValue(KEY_NB_UNCHANGED, equipmentNotModifiedCount + equipmentNotFoundCount)
                             .withSeverity(TypedValue.WARN_SEVERITY)
                             .add();
-                    report(formulaSubReporter, formulaReports);
                 }
             }
+            report(formulaSubReporter, formulaReports);
         }
     }
 
