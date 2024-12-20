04cf1beca6a0560c934b1cdcc54a525a3c33b1e3
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-12-20 11:28:57 +0100

    revert renaming for MAXIMUM_SUSCEPTANCE

--- src/test/java/org/gridsuite/modification/server/modifications/byfilter/assignment/ShuntCompensatorModificationByAssignmentTest.java
+++ src/test/java/org/gridsuite/modification/server/modifications/byfilter/assignment/ShuntCompensatorModificationByAssignmentTest.java
@@ -94,7 +94,7 @@ class ShuntCompensatorModificationByAssignmentTest extends AbstractModificationB
                 .build();
 
         DoubleAssignmentInfos assignmentInfos3 = DoubleAssignmentInfos.builder()
-                .editedField(ShuntCompensatorField.MAX_SUSCEPTANCE.name())
+                .editedField(ShuntCompensatorField.MAXIMUM_SUSCEPTANCE.name())
                 .value(5.)
                 .filters(List.of(filter4))
                 .build();
