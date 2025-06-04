80bffba161a8dea724f0928e1c4f4b33dace986a
EtienneLt <32468651+EtienneLt@users.noreply.github.com>
2025-06-04 10:20:41 +0200

    Manage coupling device creation (#635)
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

---
+++ src/main/java/org/gridsuite/modification/server/entities/equipment/creation/CreateCouplingDeviceEntity.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.modification.server.entities.equipment.creation;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.gridsuite.modification.dto.CouplingDeviceInfos;
+import org.gridsuite.modification.dto.CreateCouplingDeviceInfos;
+import org.gridsuite.modification.dto.ModificationInfos;
+import org.gridsuite.modification.server.entities.ModificationEntity;
+
+/**
+ * @author etienne Lesot <etienne.lesot at rte-france.com>
+ */
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "create_coupling_device")
+@PrimaryKeyJoinColumn(foreignKey = @ForeignKey(name = "create_coupling_device_id_fk_constraint"))
+public class CreateCouplingDeviceEntity extends ModificationEntity {
+
+    @Column(name = "voltage_level_id")
+    private String voltageLevelId;
+
+    @Column(name = "bus_or_bbs_id1")
+    private String busOrBbsId1;
+
+    @Column(name = "bus_or_bbs_id2")
+    private String busOrBbsId2;
+
+    @Override
+    public void update(@NonNull ModificationInfos modificationInfos) {
+        super.update(modificationInfos);
+        assignAttributes((CreateCouplingDeviceInfos) modificationInfos);
+    }
+
+    private void assignAttributes(CreateCouplingDeviceInfos createCouplingDeviceInfos) {
+        this.voltageLevelId = createCouplingDeviceInfos.getVoltageLevelId();
+        if (createCouplingDeviceInfos.getCouplingDeviceInfos() != null) {
+            this.busOrBbsId1 = createCouplingDeviceInfos.getCouplingDeviceInfos().getBusbarSectionId1();
+            this.busOrBbsId2 = createCouplingDeviceInfos.getCouplingDeviceInfos().getBusbarSectionId2();
+        }
+    }
+
+    public CreateCouplingDeviceEntity(CreateCouplingDeviceInfos createCouplingDeviceInfos) {
+        super(createCouplingDeviceInfos);
+        assignAttributes(createCouplingDeviceInfos);
+    }
+
+    @Override
+    public CreateCouplingDeviceInfos toModificationInfos() {
+        return toCreateCouplingDeviceInfos().build();
+    }
+
+    private CreateCouplingDeviceInfos.CreateCouplingDeviceInfosBuilder<?, ?> toCreateCouplingDeviceInfos() {
+        return CreateCouplingDeviceInfos.builder()
+            .uuid(getId())
+            .date(getDate())
+            .stashed(getStashed())
+            .activated(getActivated())
+            .voltageLevelId(getVoltageLevelId())
+            .couplingDeviceInfos(CouplingDeviceInfos.builder()
+                .busbarSectionId1(getBusOrBbsId1())
+                .busbarSectionId2(getBusOrBbsId2())
+                .build());
+    }
+}
