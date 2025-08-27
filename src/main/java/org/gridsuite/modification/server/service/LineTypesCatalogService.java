9a8f50473c57069bff66c56a4493700d19498d17
Etienne LESOT <etienne.lesot@rte-france.com>
2025-08-27 09:32:44 +0200

    fix checkstyle
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/main/java/org/gridsuite/modification/server/service/LineTypesCatalogService.java
+++ src/main/java/org/gridsuite/modification/server/service/LineTypesCatalogService.java
@@ -17,7 +17,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
 
