4a640aa43a6359817bb7919075e7bf1ed4510886
Mathieu Deharbe <148252167+Mathieu-Deharbe@users.noreply.github.com>
2025-10-08 15:20:32 +0200

    edit olg switch (adds editedOperationalLimitsGroups) (#691)
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>
    Co-authored-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/test/java/org/gridsuite/modification/server/modifications/tabularmodifications/TabularTwoWindingsTransformerModificationsTest.java
+++ src/test/java/org/gridsuite/modification/server/modifications/tabularmodifications/TabularTwoWindingsTransformerModificationsTest.java
@@ -73,6 +73,7 @@ class TabularTwoWindingsTransformerModificationsTest extends AbstractNetworkModi
     protected TwoWindingsTransformerModificationInfos buildOneModification(String equipmentId, Double seriesResistance) {
         return TwoWindingsTransformerModificationInfos.builder().equipmentId(equipmentId)
                 .r(new AttributeModification<>(seriesResistance, OperationType.SET))
+                .enableOLGModification(true)
                 .build();
     }
 
@@ -119,6 +120,7 @@ class TabularTwoWindingsTransformerModificationsTest extends AbstractNetworkModi
                         (ModificationInfos) TwoWindingsTransformerModificationInfos.builder().equipmentId(UUID.randomUUID().toString())
                                 .x(new AttributeModification<>(1., OperationType.SET))
                                 .g(new AttributeModification<>(1., OperationType.SET))
+                                .enableOLGModification(true)
                                 .operationalLimitsGroups(TabularLineModificationsTest.buildOperationalLimitsGroupDefaultModification())
                                 .properties(List.of(
                                         ModificationCreation.getFreeProperty(),
