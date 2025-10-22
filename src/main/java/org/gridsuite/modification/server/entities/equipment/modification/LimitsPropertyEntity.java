b02d6f2a6c18d0e1296f805ccc9e20dd17df0a41
basseche <bassel.el-cheikh_externe@rte-france.com>
2025-10-22 13:25:30 +0200

    add copyright and author

--- src/main/java/org/gridsuite/modification/server/entities/equipment/modification/LimitsPropertyEntity.java
+++ src/main/java/org/gridsuite/modification/server/entities/equipment/modification/LimitsPropertyEntity.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package org.gridsuite.modification.server.entities.equipment.modification;
 
 import jakarta.persistence.*;
@@ -8,6 +15,10 @@ import lombok.Setter;
 import org.gridsuite.modification.dto.LimitsPropertyInfos;
 
 import java.util.UUID;
+
+/**
+ * @author basseche <bassel.el-cheikh_externe at rte-france.com>
+ */
 
 @Getter
 @Setter
