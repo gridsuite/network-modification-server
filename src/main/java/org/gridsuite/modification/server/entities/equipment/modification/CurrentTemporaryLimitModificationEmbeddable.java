6da139568e4bbf6d88a006fbc9b03f88d9c56c6d
Hugo Marcellin <hugo.marcelin@rte-france.com>
2025-12-05 14:45:13 +0100

    Implement operationType support for temporary limits

--- src/main/java/org/gridsuite/modification/server/entities/equipment/modification/CurrentTemporaryLimitModificationEmbeddable.java
+++ src/main/java/org/gridsuite/modification/server/entities/equipment/modification/CurrentTemporaryLimitModificationEmbeddable.java
@@ -9,13 +9,16 @@ package org.gridsuite.modification.server.entities.equipment.modification;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.gridsuite.modification.dto.AttributeModification;
 import org.gridsuite.modification.dto.CurrentTemporaryLimitModificationInfos;
+import org.gridsuite.modification.dto.OperationType;
 import org.gridsuite.modification.dto.TemporaryLimitModificationType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import java.util.Collections;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -32,27 +35,59 @@ public class CurrentTemporaryLimitModificationEmbeddable {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "nameOp")
+    @Enumerated(EnumType.STRING)
+    private OperationType nameOp;
+
     @Column(name = "value_")
     private Double value;
 
+    @Column(name = "valueOp")
+    @Enumerated(EnumType.STRING)
+    private OperationType valueOp;
+
     @Column(name = "acceptableDuration")
     private Integer acceptableDuration;
+
+    @Column(name = "acceptableDurationOp")
+    @Enumerated(EnumType.STRING)
+    private OperationType acceptableDurationOp;
 
     @Column(name = "modificationType")
     @Enumerated(EnumType.STRING)
     private TemporaryLimitModificationType modificationType;
 
     public static List<CurrentTemporaryLimitModificationEmbeddable> toEmbeddableCurrentTemporaryLimits(List<CurrentTemporaryLimitModificationInfos> limits) {
-        return limits == null ? null :
-                limits.stream()
-                        .map(limit -> new CurrentTemporaryLimitModificationEmbeddable(limit.getName(), limit.getValue(), limit.getAcceptableDuration(), limit.getModificationType()))
-                        .collect(Collectors.toList());
+        if (limits == null) {
+            return Collections.emptyList();
+        }
+        return limits.stream()
+                .map(limit -> {
+                    String nameValue = limit.getName() != null ? limit.getName().getValue() : null;
+                    OperationType nameOp = limit.getName() != null ? limit.getName().getOp() : null;
+                    Double valueValue = limit.getValue() != null ? limit.getValue().getValue() : null;
+                    OperationType valueOp = limit.getValue() != null ? limit.getValue().getOp() : null;
+                    Integer durationValue = limit.getAcceptableDuration() != null ? limit.getAcceptableDuration().getValue() : null;
+                    OperationType durationOp = limit.getAcceptableDuration() != null ? limit.getAcceptableDuration().getOp() : null;
+                    return new CurrentTemporaryLimitModificationEmbeddable(
+                            nameValue,
+                            nameOp,
+                            valueValue,
+                            valueOp,
+                            durationValue,
+                            durationOp,
+                            limit.getModificationType());
+                }).toList();
     }
 
     public static List<CurrentTemporaryLimitModificationInfos> fromEmbeddableCurrentTemporaryLimits(List<CurrentTemporaryLimitModificationEmbeddable> limits) {
         return limits == null ? null :
                 limits.stream()
-                        .map(limit -> new CurrentTemporaryLimitModificationInfos(limit.getName(), limit.getValue(), limit.getAcceptableDuration(), limit.getModificationType()))
-                        .collect(Collectors.toList());
+                        .map(limit -> new CurrentTemporaryLimitModificationInfos(
+                                AttributeModification.toAttributeModification(limit.getName(), limit.getNameOp()),
+                                AttributeModification.toAttributeModification(limit.getValue(), limit.getValueOp()),
+                                AttributeModification.toAttributeModification(limit.getAcceptableDuration(), limit.getAcceptableDurationOp()),
+                                limit.getModificationType()))
+                        .toList();
     }
 }
