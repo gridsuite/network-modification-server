a5879ad271adfe4a60e6700eb89d229d220ae02d
Hugo Marcellin <hugo.marcelin@rte-france.com>
2026-03-27 10:19:42 +0100

    Add composite interactions

--- src/main/java/org/gridsuite/modification/server/entities/CompositeModificationEntity.java
+++ src/main/java/org/gridsuite/modification/server/entities/CompositeModificationEntity.java
@@ -32,7 +32,7 @@ public class CompositeModificationEntity extends ModificationEntity {
     @ColumnDefault("'My Composite'")
     private String name;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name = "compositeModificationSubModifications",
             joinColumns = @JoinColumn(name = "id"), foreignKey = @ForeignKey(name = "composite_modification_sub_modifications_id_fk"),
