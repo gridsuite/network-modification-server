2f693e3b986af8ca68e3d9537bc5b464838241bf
Hugo Marcellin <hugo.marcelin@rte-france.com>
2026-04-13 10:51:13 +0200

    Licence

--- src/main/java/org/gridsuite/modification/server/repositories/CompositeModificationRepository.java
+++ src/main/java/org/gridsuite/modification/server/repositories/CompositeModificationRepository.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.modification.server.repositories;
 
 import org.gridsuite.modification.server.entities.CompositeModificationEntity;
@@ -7,6 +13,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
+
+/**
+ * @author Hugo Marcellin <hugo.marcelin at rte-france.com>
+ */
 
 public interface CompositeModificationRepository extends JpaRepository<CompositeModificationEntity, UUID> {
 
