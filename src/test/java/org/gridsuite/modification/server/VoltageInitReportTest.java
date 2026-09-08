322a4572a016584e972a36d9cb310540727e2917
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2026-09-08 14:18:50 +0200

    VoltageInitReportTest
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/test/java/org/gridsuite/modification/server/VoltageInitReportTest.java
+++ src/test/java/org/gridsuite/modification/server/VoltageInitReportTest.java
@@ -111,8 +111,11 @@ class VoltageInitReportTest {
             Arguments.of(ApplicationStatus.ALL_OK, "reports_voltage_init_modification_ok.json", VoltageInitModificationInfos.builder()
                 .uuid(UUID.fromString("44444444-4444-4444-4444-444444444444"))
                 .date(Instant.EPOCH)
-                .stashed(false)
-                .batteries(List.of())// TODO à remplir
+                .batteries(List.of(
+                    VoltageInitBatteryModificationInfos.builder()
+                        .batteryId("ESQUE.HG1")
+                        .targetQ(10.)
+                        .build()))
                 .generators(List.of(
                     VoltageInitGeneratorModificationInfos.builder().generatorId("GTH2").targetV(0.1).build())) //added for test case
                 .transformers(List.of(
