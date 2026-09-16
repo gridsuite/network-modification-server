f6b27828838d3f9dc8bb3ede3fa48dcb0a5497f2
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2026-09-16 16:27:50 +0200

    remove directoryService
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/main/java/org/gridsuite/modification/server/service/NetworkModificationService.java
+++ src/main/java/org/gridsuite/modification/server/service/NetworkModificationService.java
@@ -81,8 +81,6 @@ public class NetworkModificationService {
 
     private final FilterService filterService;
 
-    private final DirectoryService directoryService;
-
     static final String NETWORK_UUID = "networkUuid.keyword";
     static final String CREATED_EQUIPMENT_IDS = "createdEquipmentIds.fullascii";
     static final String MODIFIED_EQUIPMENT_IDS = "modifiedEquipmentIds.fullascii";
@@ -99,8 +97,7 @@ public class NetworkModificationService {
                                       ModificationApplicationInfosService applicationInfosService,
                                       ElasticsearchOperations elasticsearchOperations,
                                       ModificationRepository modificationRepository,
-                                      FilterService filterService,
-                                      DirectoryService directoryService) {
+                                      FilterService filterService) {
         this.networkStoreService = networkStoreService;
         this.networkModificationRepository = networkModificationRepository;
         this.equipmentInfosService = equipmentInfosService;
@@ -111,7 +108,6 @@ public class NetworkModificationService {
         this.elasticsearchOperations = elasticsearchOperations;
         this.modificationRepository = modificationRepository;
         this.filterService = filterService;
-        this.directoryService = directoryService;
     }
 
     public List<UUID> getModificationGroups() {
