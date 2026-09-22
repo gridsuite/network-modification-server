d473b2a1dfc911f0b7f77ad2e44f9b43f826f7b1
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2026-09-22 14:49:28 +0200

    sonar
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/main/java/org/gridsuite/modification/server/service/NetworkModificationService.java
+++ src/main/java/org/gridsuite/modification/server/service/NetworkModificationService.java
@@ -608,20 +608,19 @@ public class NetworkModificationService {
 
         // update the references whose container is now the new composite (and the root container is the node)
         List<ModificationReferenceData> references = getModificationsReferencesNonTransactional(assembledModificationsUuids, false);
-        references.forEach(ref ->
-                {
-                    ReferenceAttributes referenceAttributes = ReferenceAttributes.createReferenceAttributes(
-                            ref.modificationUuid(),
-                            nodeUuid,
-                            newComposite.getUuid(),
-                            ReferenceAttributes.ReferenceType.STUDY_NODE_NETWORK_MODIFICATION
-                    );
-                    directoryService.updateElementReference(
-                            ref.referencedId(),
-                            referenceAttributes,
-                            userId
-                    );
-                }
+        references.forEach(ref -> {
+            ReferenceAttributes referenceAttributes = ReferenceAttributes.createReferenceAttributes(
+                    ref.modificationUuid(),
+                    nodeUuid,
+                    newComposite.getUuid(),
+                    ReferenceAttributes.ReferenceType.STUDY_NODE_NETWORK_MODIFICATION
+            );
+            directoryService.updateElementReference(
+                    ref.referencedId(),
+                    referenceAttributes,
+                    userId
+            );
+        }
         );
 
         return newComposite.getUuid();
